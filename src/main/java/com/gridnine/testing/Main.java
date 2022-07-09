2833251438a2a44a6ee9e0a0cbdc5f9599fa3759
”konstantin <vihroff@gmail.com>
2022-07-09 14:10:26 +0300

    Initial commit

---
+++ src/main/java/com/gridnine/testing/Main.java
@@ -0,0 +1,15 @@
+package com.gridnine.testing;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main
+{
+    public static void main( String[] args )
+    {
+
+
+        System.out.println( "Hello World!" );
+    }
+}
